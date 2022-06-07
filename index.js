ad724a7e073add7a960d338864c3717864d90ac8
Zachary <zacharylampi96@gmail.com>
2022-06-07 17:33:10 -0400

    commit tree 89869a3ff8c46424dad40791147b1e4ee38755d0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here!;
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.setAttribute("h1", "victory");
+newHeader.textContent = "Zack is the champion"
